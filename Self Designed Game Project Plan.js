31e5cb0a6fc4f98b5eddca25295d8772e5b39833
Palak03122007 <76035747+Palak03122007@users.noreply.github.com>
2021-08-11 14:38:10 +0530

    Add files via upload

---
+++ Self Designed Game Project Plan.js
@@ -0,0 +1,49 @@
+Game Design Document
+Fill up the following document 
+
+________________________________________
+
+
+1.	Write the title of your project.
+
+Catch The Butterflies
+
+
+2.	What is the goal of the game? 
+
+A girl has to catch all the butterflies clearing all the obstacles.
+
+
+3.	Write a brief story of your game.
+
+There is a girl who is really fond of butterflies. Hence, she goes in a jungle to grab butterflies. There, she has to face all the obstacles to catch the butterflies.
+
+
+4.	Which are the playing characters of this game? 
+●	Playing characters are the ones who respond to the user based on the input from the user.
+●	The girl is the playing characters in the game.  
+
+Number	Character Name	What can this character do?
+1	Girl	It can run and jump
+
+
+5.	Which are the Non-Playing Characters of this game?
+●	Non-Playing characters are the ones that don't have an action or behavior when the user interacts with the game.
+●	Butterflies, trees and animals etc., are non-playing characters in the game.   
+
+Number	Character Name	What can this character do?
+1	Butterflies	They are like the points/coins which the girl has to catch
+2	Trees	These are the obstacles
+3	Animals	They can move and make the player lose
+
+
+Draw your imagination of this game. What does this game look like?
+●	Draw the game either on your computer or on paper. 
+●	Add images of the game scenes to show each of the playing and non-playing characters at least once.  
+
+
+ 
+
+
+How do you plan to make your game engaging? 
+There would be many obstacles, so the player would be engaged in the game. It won’t be too easy, or too hard either.
